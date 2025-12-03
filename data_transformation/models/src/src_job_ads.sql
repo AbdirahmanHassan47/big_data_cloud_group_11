9a0b09bac639fa690425fe09b4ada54f1954f338
abdirahman hassan <abdi1.hassan@icloud.com>
2025-12-03 02:13:30 +0100

    update 2

---
+++ data_transformation/models/src/src_job_ads.sql
@@ -0,0 +1,13 @@
+    with stg_job_ads as (select * from {{ source('job_ads', 'stg_ads') }} )
+
+    
+    SELECT
+        id,
+        headline,
+        occupation__label,
+        number_of_vacancies as vacancies,
+        relevance,
+        application_deadline
+    FROM stg_job_ads
+    order by application_deadline
+    
